8647a5b358bda3e33ee2b47c825eaddd0278b6fb
khalil el moutee <elmouteekhalil@gmail.com>
2024-01-07 08:20:20 -0800

    khalilll

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from hidden_4 import *
+    all_fuction = dir ()
+    for i in range(0, len(all_fuction)):
+        if all_fuction[i][:2] != "__":
+            print ("".format(all_fuction[i]))
